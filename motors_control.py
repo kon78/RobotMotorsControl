c7072295ed327cc9e9edc4799193ea9d0d1cb424
kon78 <misterqjr@mail.ru>
2025-01-23 19:18:39 +0300

    Added control for 2 motors

--- motors_control.py
+++ motors_control.py
@@ -11,6 +11,7 @@ print ("Set GPIO line")
 HIGH=1 #TRUE
 LOW=0 #FALSE
 
+#left motor
 # Open GPIO /dev/gpiochip1 line 11 direction in/out
 #Motor1A --> 11 #GPIO Linux 876
 Motor1A = GPIO("/dev/gpiochip1", 11, "out")
@@ -19,10 +20,23 @@ Motor1B = GPIO("/dev/gpiochip1", 13, "out")
 #Motor1E --> 15 #925
 Motor1E = GPIO("/dev/gpiochip1", 15, "out")
 
+#right motor
+#Motors2A --> 19 #GPIO Linux 871
+Motor2A = GPIO("/dev/gpiochip1", 19, "out")
+#Motors2B --> 21 #GPIO Linux 870
+Motor2B = GPIO("/dev/gpiochip1", 21, "out")
+#Motors2E --> 23 #GPIO Linux 867
+Motor2E = GPIO("/dev/gpiochip1", 23, "out")
+
+
 print ("Going forwards")
 Motor1A.write(bool(HIGH))
 Motor1B.write(bool(LOW))
 Motor1E.write(bool(HIGH))
+
+Motor2A.write(bool(HIGH))
+Motor2B.write(bool(LOW))
+Motor2E.write(bool(HIGH))
 
 sleep(2)
 print ("Going backwards")
@@ -30,12 +44,21 @@ Motor1A.write(bool(LOW))
 Motor1B.write(bool(HIGH))
 Motor1E.write(bool(HIGH))
 
+Motor2A.write(bool(LOW))
+Motor2B.write(bool(HIGH))
+Motor2E.write(bool(HIGH))
+
 sleep(2)
 print ("Now stop!")
 Motor1E.write(bool(LOW))
+Motor2E.write(bool(LOW))
 
+#free
 Motor1A.close()
 Motor1B.close()
 Motor1E.close()
+Motor2A.close()
+Motor2B.close()
+Motor2E.close()
 
 #end
